8e992f41b5ccc0815c437b0938d358939bc5e3be
Brian Hague <alphaseinor@gmail.com>
2019-12-06 21:35:13 -0600

    added locked and unlocked tests to displayspec

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -8,5 +8,25 @@ describe('<Display />', () => {
     expect(display).toMatchSnapshot()
   })
 
+  test('if true, display closed', () => {
+    const { getByText } = render(<Display closed={true} />)
+    expect(getByText(/closed/i))
+  })
+    
+  test('if false, show open', () => {
+    const { getByText } = render(<Display closed={false} />)
+    expect(getByText(/open/i))
+  })
+    
+  test('if true, show locked', () => {
+    const { getByText } = render(<Display locked={true} />)
+    expect(getByText(/locked/i))
+  })
+    
+  test('if false, show unlocked', () => {
+    const { getByText } = render(<Display locked={false} />)
+    expect(getByText(/unlocked/i))
+  })
 
+ 
 })
