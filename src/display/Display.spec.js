11dd402aa10ba0432d7c45a476219a66ea6ed259
Brian Hague <alphaseinor@gmail.com>
2019-12-06 21:30:20 -0600

    added snapshot test to displayspec

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react'
+import { render } from '@testing-library/react'
+import Display from '../display/Display'
+
+describe('<Display />', () => {
+  test('Snapshot test', () => {
+    const display = render('<Display />')
+    expect(display).toMatchSnapshot()
+  })
+
+
+})
